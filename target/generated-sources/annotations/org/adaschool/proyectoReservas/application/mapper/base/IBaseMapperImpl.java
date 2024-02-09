9098d50808f67cd4d2e6856a48291eb026ef77c5
Maria <mapis2321@gmail.com>
2024-02-09 14:12:59 -0500

    Modificaciones finales

--- target/generated-sources/annotations/org/adaschool/proyectoReservas/application/mapper/base/IBaseMapperImpl.java
+++ target/generated-sources/annotations/org/adaschool/proyectoReservas/application/mapper/base/IBaseMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-01-19T12:03:13-0500",
+    date = "2024-02-09T14:11:47-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
 )
 @Component
